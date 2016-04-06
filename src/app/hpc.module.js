bae6ef565292643d714fe00f97da80806af3aa36
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2016-04-06 08:40:15 -0500

    Archivo de modulo principal de la aplicacion

---
+++ src/app/hpc.module.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict'
+
+    angular.module('hpc', [
+
+        /** Shared modules **/
+        'hpc.core',
+
+        /** Feature modules **/
+        'hpc.login'
+    ]);
+})();
