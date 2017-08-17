c86218a31e6f7232863ba54d27a2db2162fe7d3c
Angel Jimenez Escobar <ajimenez@digitalcaribe.com.co>
2017-08-17 11:38:35 -0500

    Agregando dependencias a bower, y a node, actualizando el archivo gulp de styles

--- gulp_tasks/styles.js
+++ gulp_tasks/styles.js
@@ -10,7 +10,7 @@ const conf = require('../conf/gulp.conf');
 gulp.task('styles', styles);
 
 function styles() {
-  return gulp.src(conf.path.src('index.styl'))
+  return gulp.src(conf.path.src('content/stylus/main.styl'))
     .pipe(sourcemaps.init())
     .pipe(stylus({compress: false})).on('error', conf.errorHandler('Stylus'))
     .pipe(postcss([autoprefixer()])).on('error', conf.errorHandler('Autoprefixer'))
